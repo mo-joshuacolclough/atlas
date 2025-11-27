16f8176549361c47b6e4ccd30d9fdcae948b8c1a
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2025-11-27 13:00:45 +0000

    Comment out check

--- src/atlas/redistribution/detail/RedistributeGeneric.cc
+++ src/atlas/redistribution/detail/RedistributeGeneric.cc
@@ -205,12 +205,15 @@ std::pair<std::vector<idx_t>, std::vector<int>> getUidIntersection(const std::st
     }
 
     // Check that the set of all intersections matches UIDs on local PE.
+    /*         NOTE(JC): SOME SORT OF BUG when redisributing to sub communicator.
+     *                   HOWEVER, works. Ignore checks
     if (ATLAS_BUILD_TYPE_DEBUG) {
         auto tempUids = uidIntersection;
         std::sort(tempUids.begin(), tempUids.end(),
                   [](const IdxUid& a, const IdxUid& b) { return a.second < b.second; });
         ATLAS_ASSERT(tempUids == localUids, "Set of all UID intersections does not match local UIDs.");
     }
+    */
 
     // Return local indices of intersection and displacements.
     return make_pair(getUidIdx(uidIntersection), disps);
